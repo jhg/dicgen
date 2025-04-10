853e623c6857d8e1d34d2bd8a4c661c874cfa4a4
Jesus Hernandez <jesushdez@protonmail.com>
2025-04-10 14:05:05 +0200

    Fix last fix

--- src/lib.rs
+++ src/lib.rs
@@ -156,6 +156,7 @@ impl DictionaryGenerator {
     pub fn reset_starting_in<I: AsRef<str>>(&mut self, init: I) {
         if let Some(mut current_value) = self.current_value.take() {
             current_value.clear();
+            current_value.extend(init.as_ref().chars().rev());
             self.current_value = Some(current_value);
             return;
         }
